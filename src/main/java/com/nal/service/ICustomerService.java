c994b29aea77e98621cd7a22596e47a4175f85a5
KatoIT <nguyenvananymnb@gmail.com>
2022-02-17 15:41:44 +0700

    [Thực hành] Sử dụng thymeleaf cho ứng dụng Quản lý Dữ liệu Khách hàng

---
+++ src/main/java/com/nal/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.nal.service;
+
+import com.nal.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
