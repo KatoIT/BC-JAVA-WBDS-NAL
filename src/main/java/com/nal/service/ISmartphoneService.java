c80bec0d3c8bb2017ad00da35ef5a92d634a3ba3
KatoIT <nguyenvananymnb@gmail.com>
2022-02-25 10:26:38 +0700

    [Bài tập] Hoàn thiện ứng dụng quản lý điện thoại

---
+++ src/main/java/com/nal/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package com.nal.service;
+
+import com.nal.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
