018d1fa78ac621f710fc2933b9d60880dd8b8b52
KatoIT <nguyenvananymnb@gmail.com>
2022-02-25 13:19:50 +0700

    [Thực hành] Sử dụng Spring Security

---
+++ src/main/java/com/nal/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.nal.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
