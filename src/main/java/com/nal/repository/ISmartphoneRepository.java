870f4d8f76e2a8afbfc1b5c9da893547ba81d9af
KatoIT <nguyenvananymnb@gmail.com>
2022-02-25 09:28:16 +0700

    [Thực hành] Quản lý điện thoại

---
+++ src/main/java/com/nal/repository/ISmartphoneRepository.java
@@ -0,0 +1,9 @@
+package com.nal.repository;
+
+import com.nal.model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
