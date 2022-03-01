f35d40d52fbd8946228442f470576c7680641de4
KatoIT <nguyenvananymnb@gmail.com>
2022-03-01 16:10:52 +0700

    [Thực hành] Spring Boot JSON Web Token (JWT)Page

---
+++ Spring-Tutorial/src/main/java/com/nal/security/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,64 @@
+package com.nal.security;
+
+import com.nal.service.JwtService;
+import com.nal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtAuthenticationTokenFilter extends UsernamePasswordAuthenticationFilter {
+    private final static String TOKEN_HEADER = "authorization";
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    @Override
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String authToken = getJwtFromRequest(httpRequest.getHeader(TOKEN_HEADER));
+        if (jwtService.validateTokenLogin(authToken)) {
+            String username = jwtService.getUsernameFromToken(authToken);
+            com.nal.model.User user = userService.loadUserByUsername(username);
+            if (user != null) {
+                boolean enabled = true;
+                boolean accountNonExpired = true;
+                boolean credentialsNonExpired = true;
+                boolean accountNonLocked = true;
+                UserDetails userDetail = new User(username, user.getPassword(), enabled, accountNonExpired,
+                        credentialsNonExpired, accountNonLocked, user.getAuthorities());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetail,
+                        null, userDetail.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+    private String getJwtFromRequest(String authHeader) {
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
+
