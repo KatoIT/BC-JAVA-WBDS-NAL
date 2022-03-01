bb5e238ca8b5928022ca99456de5477e972bbd11
KatoIT <nguyenvananymnb@gmail.com>
2022-03-01 11:01:05 +0700

    [Thực hành] Ứng dụng quản lý khách hàng sử dụng Spring Boot

---
+++ Spring-Tutorial/src/main/java/com/nal/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.nal.repository;
+
+import com.nal.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ Spring-Tutorial/src/main/java/com/nal/service/cutomer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.nal.service.cutomer;
+
+import com.nal.model.Customer;
+import com.nal.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
